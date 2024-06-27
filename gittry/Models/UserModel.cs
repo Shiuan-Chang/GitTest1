036518fceced14a08ac01452a8064d8845462ba0
周政輝 <leo604leo604@gmail.com>
2024-06-27 22:26:59 +0800

    finish UserModel

--- gittry/Models/UserModel.cs
+++ gittry/Models/UserModel.cs
@@ -2,6 +2,17 @@ namespace gittry.Models
 {
     public class UserModel
     {
-        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+
+        public UserModel(string Name, string Email, string Address, string Password)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Address = Address;
+            this.Password = Password;
+        }
     }
 }
