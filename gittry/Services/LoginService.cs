ed2fb1e315fb8f719473ea102e63e6de23f3a37e
MSI\icewi <icewindflyhero@gmail.com>
2024-06-27 22:52:21 +0800

    login update3

--- gittry/Services/LoginService.cs
+++ gittry/Services/LoginService.cs
@@ -4,12 +4,16 @@ namespace gittry.Services
 {
     internal class LoginService
     {
+        
+        
         public bool Login(UserModel userModel)
         {
-            if (userModel.Name == "Leo" && userModel.Email == "leo604leo604@gmail.com" && userModel.Password == "123")
+            if (userModel.Name == "Leo" && userModel.Email == "leo604leo604@gmail.com" && userModel.Password == "abc")
                 return true;
             return false;
         }
+
+
 
     }
 }
